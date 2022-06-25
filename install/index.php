e96bf8094380b9914ba89217a12e7251d7d71d4f
Kerry Atkinson <katkinson@kgaconsulting.ca>
2022-06-25 15:41:53 -0600

    Install page built

--- install/index.php
+++ install/index.php
@@ -12,114 +12,122 @@
         </tr>
         </table>
         <ul class="tabs">
-
             <li class="tab">
                 <input type="radio" name="tabs" checked="checked" id="tab1" />
-                    <label for="tab1">1.  Configuration</label>
-                    <div id="tab-content1" class="content">
-                        <h3>Main Configuration</h3>
-                        <hr />
-                        <table width="100%">
-                            <tr>
-                                <td valign="top">
-                                    <table width="100%">
-                                        <tr>
-                                            <td colspan="2">&nbsp;</td>
-                                        </tr>
-                                        <tr>
-                                            <td align="right">Company Name:</td>
-                                            <td align="left"><input type="text" id="comp_name" name="comp_name"></td>
-                                        </tr>
-                                        <tr>
-                                            <td>&nbsp;</td>
-                                            <td align="left" style="font-size:12px">Enter your company name&nbsp;</td>
-                                        </tr>
-                                    </table>
-                                </td>
-                                <td>
-                                    <table width="100%">
-                                        <tr>
-                                            <td colspan="2">&nbsp;</td>
-                                        </tr>
-                                        <tr>
-                                            <td colspan="2"><h4>System Administrator Account Details</h4></td>
-                                        </tr>
-                                        <tr>
-                                            <td colspan="2">&nbsp;</td>
-                                        </tr>
-                                        <tr>
-                                            <td align="right">Email:</td>
-                                            <td align="left"><input type="text" id="adm_email" name="adm_email"></td>
-                                        </tr>
-                                        <tr>
-                                            <td>&nbsp;</td>
-                                            <td align="left" style="font-size:12px">Enter the system administrators email address.<br />  This will be the Super User of the site</td>
-                                        </tr>
-                                        <tr>
-                                            <td colspan="2">&nbsp;</td>
-                                        </tr>
-                                        <tr>
-                                            <td align="right">Username:</td>
-                                            <td align="left"><input type="text" id="adm_username" name=adm_username></td>
-                                        </tr>
-                                        <tr>
-                                            <td>&nbsp;</td>
-                                            <td align="left" style="font-size:12px">Set the username for the system administrator.</td>
-                                        </tr>
-                                        <tr>
-                                            <td>&nbsp;</td>
-                                        </tr>
-                                        <tr>
-                                            <td align="right">Password:</td>
-                                            <td align="left"><input type="password" id="adm_password" name="adm_password"></td>
-                                        </tr>
-                                        <tr>
-                                            <td>&nbsp;</td>
-                                            <td align="left" style="font-size:12px">Set the password for the system administrator.</td>
-                                        </tr>
-                                        <tr>
-                                            <td colspan="2">&nbsp;</td>
-                                        </tr>
-                                        <tr>
-                                            <td align="right">Confirm Password:</td>
-                                            <td align="left"><input type="password" id="adm_password1" name="adm_password1"</td>
-                                        </tr>
-                                        <tr>
-                                            <td>&nbsp;</td>
-                                            <td align="left" style="font-size:12px">Confirmthe password for the system administrator.</td>
-                                        </tr>
-										<tr>
-											<td>&nbsp;</td>
-											<td align="right" style="font-size:12px"><button type="button" >Next</button></td>
-										</tr>
-                                    </table>
-                                </td>
-                            </tr>
-                        </table>
-                    </div>
-            </li>
-
-            <li class="tab">
-              <input type="radio" name="tabs" id="tab2" />
-              <label for="tab2">2.  Database</label>   
-                <div id="tab-content2" class="content">
-                     <h3>Database Configuration</h3>
-                     <hr />
-					 Put Database Configurations Here
-				</div>
-            </li>
-
-             <li class="tab">
-              <input type="radio" name="tabs" id="tab3" />
-              <label for="tab3">3.  Overview</label>   
-                  <div id="tab-content3" class="content">
-                    <h3>Finalization</h3>
+                <label for="tab1">Configuration</label>
+                <div id="tab-content1" class="content">
+                    <h3 align="center">Main Configuration</h3>
                     <hr />
-                    <h3>Overview</h3>
-                    <hr />
-                  </div>
+                    <table width="100%">
+                        <tr>
+                            <td valign="top">
+                                <table width="100%">
+                                    <tr>
+                                        <td colspan="2">&nbsp;</td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="2"><h4>Database setup</h4></td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="2">&nbsp;</td>
+                                    </tr>
+                                    <tr>
+                                        <td align="right">Database Name:</td>
+                                        <td align="left"><input type="text" id="db_name" name="db_name"></td>
+                                    </tr>
+                                    <tr>
+                                        <td>&nbsp;</td>
+                                        <td align="left" style="font-size:12px">Enter the name of your new database&nbsp;</td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="2">&nbsp;</td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="2">&nbsp;</td>
+                                    </tr>
+                                    <tr>
+                                        <td align="right">Database User Name:</td>
+                                        <td align="left"><input type="text" id="db_user" name="db_user"></td>
+                                    </tr>
+                                    <tr>
+                                        <td>&nbsp;</td>
+                                        <td align="left" style="font-size:12px">Enter the name of your database user&nbsp;</td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="2">&nbsp;</td>
+                                    </tr>
+                                    <tr>
+                                        <td align="right">Database User Password:</td>
+                                        <td align="left"><input type="text" id="db_pass" name="db_pass"></td>
+                                    </tr>
+                                    <tr>
+                                        <td>&nbsp;</td>
+                                        <td align="left" style="font-size:12px">Enter the password for your database user&nbsp;</td>
+                                    </tr>
+                                </table>
+                            </td>
+                            <td>
+                                <table width="100%">
+                                    <tr>
+                                        <td colspan="2">&nbsp;</td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="2"><h4>System Administrator Account Details</h4></td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="2">&nbsp;</td>
+                                    </tr>
+                                    <tr>
+                                        <td align="right">Email:</td>
+                                        <td align="left"><input type="text" id="adm_email" name="adm_email"></td>
+                                    </tr>
+                                    <tr>
+                                        <td>&nbsp;</td>
+                                        <td align="left" style="font-size:12px">Enter the system administrators email address.<br />  This will be the Super User of the site</td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="2">&nbsp;</td>
+                                    </tr>
+                                    <tr>
+                                        <td align="right">Username:</td>
+                                        <td align="left"><input type="text" id="adm_username" name=adm_username></td>
+                                    </tr>
+                                    <tr>
+                                        <td>&nbsp;</td>
+                                        <td align="left" style="font-size:12px">Set the username for the system administrator.</td>
+                                    </tr>
+                                    <tr>
+                                        <td>&nbsp;</td>
+                                    </tr>
+                                    <tr>
+                                        <td align="right">Password:</td>
+                                        <td align="left"><input type="password" id="adm_password" name="adm_password"></td>
+                                    </tr>
+                                    <tr>
+                                        <td>&nbsp;</td>
+                                        <td align="left" style="font-size:12px">Set the password for the system administrator.</td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="2">&nbsp;</td>
+                                    </tr>
+                                    <tr>
+                                        <td align="right">Confirm Password:</td>
+                                        <td align="left"><input type="password" id="adm_password1" name="adm_password1"</td>
+                                    </tr>
+                                    <tr>
+                                        <td>&nbsp;</td>
+                                        <td align="left" style="font-size:12px">Confirmthe password for the system administrator.</td>
+                                    </tr>
+                                    <tr>
+                                        <td>&nbsp;</td>
+                                        <td align="right" style="font-size:12px"><button type="button" >Next</button></td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
             </li>
-
-          </ul>
+        </ul>    
     </body>
 </html>
